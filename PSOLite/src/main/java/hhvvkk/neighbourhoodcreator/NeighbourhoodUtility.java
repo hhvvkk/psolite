a9612e3b8b216d0105f0a1b6e9d6d52267cf0edf
Henko van Koesveld <hvkoesveld@gmail.com>
2014-02-08 12:42:04 +0200

    added progress for neighbourhood creator

--- PSOLite/src/main/java/hhvvkk/neighbourhoodcreator/NeighbourhoodUtility.java
+++ PSOLite/src/main/java/hhvvkk/neighbourhoodcreator/NeighbourhoodUtility.java
@@ -4,9 +4,45 @@
  */
 package hhvvkk.neighbourhoodcreator;
 
+import java.util.LinkedList;
+
 /**
  * A class to create neighborhoods for the particles in order to create a specialized topology
  */
 public class NeighbourhoodUtility {
+    
         
+        /**
+         * Constructs a neighborhood of particles of size specified
+         * @param numberOfParticles The amount of particles in the neighborhood
+         */
+        NeighbourhoodUtility(int numberOfParticles){
+                
+        }
+        LinkedList<Object>;
+      
+        /**
+         * Adds a particle as a neighbor of 
+         * @param indexOfParticle
+         * @param indexOf 
+         */
+        public void addNeigbour(int indexOfParticle, int indexOf){
+                
+        }
+        
+        /**
+         * Removes a particle from a particle neighborhood
+         * @param indexOfParticle 
+         */
+        public void removeParticle(int indexOfParticle){
+                
+        }
+        
+        /**
+         * Builds and returns a string representation of the neighborhoods of each particle
+         * @return Return a string form of all particle neighborhoods
+         */
+        public String stringifyNeighbourhood(){
+                return "";
+        }
 }
