ff50f18cf1c1a7f711d6f6136a134e17225ace52
Henko van Koesveld <hvkoesveld@gmail.com>
2014-02-10 00:09:53 +0200

    Progress to neighbourhood util

--- PSOLite/src/main/java/hhvvkk/neighbourhoodcreator/NeighbourhoodUtility.java
+++ PSOLite/src/main/java/hhvvkk/neighbourhoodcreator/NeighbourhoodUtility.java
@@ -4,45 +4,64 @@
  */
 package hhvvkk.neighbourhoodcreator;
 
+import java.util.ArrayList;
 import java.util.LinkedList;
 
 /**
  * A class to create neighborhoods for the particles in order to create a specialized topology
  */
 public class NeighbourhoodUtility {
-    
+        //The particles with their
+        private ArrayList<LinkedList<Integer>>particles = null;
+      
         
         /**
          * Constructs a neighborhood of particles of size specified
          * @param numberOfParticles The amount of particles in the neighborhood
          */
-        NeighbourhoodUtility(int numberOfParticles){
+        public NeighbourhoodUtility(int numberOfParticles){
+                particles = new ArrayList<LinkedList<Integer>>(numberOfParticles);
                 
+                for(int i = 0; i < numberOfParticles; i++){
+                        //add all particles with no neighbors initially
+                        particles.add(new LinkedList<Integer>());
+                }
         }
-        LinkedList<Object>;
-      
+        
         /**
-         * Adds a particle as a neighbor of 
-         * @param indexOfParticle
-         * @param indexOf 
+         * Get the amount of particles
+         * @return Return the amount of particles
          */
-        public void addNeigbour(int indexOfParticle, int indexOf){
-                
+        public int getSize(){
+                return particles.size();
+        }
+        
+        /**
+         * Adds a particle as a neighbor of another particle
+         * @param particleIndex
+         * @param particleNeighbourIndex
+         */
+        public void addNeigbour(int particleIndex, int particleNeighbourIndex){
+                particles.get(particleIndex).add(particleNeighbourIndex);
         }
         
         /**
          * Removes a particle from a particle neighborhood
-         * @param indexOfParticle 
+         * @param indexOfParticle : The particle to remove from the list
          */
         public void removeParticle(int indexOfParticle){
-                
+                particles.remove(indexOfParticle);
         }
         
         /**
-         * Builds and returns a string representation of the neighborhoods of each particle
-         * @return Return a string form of all particle neighborhoods
+         * Gets the particle linked list showing
+         * @return Returns a new array list containing the same particle neighborhoods as created
          */
-        public String stringifyNeighbourhood(){
-                return "";
+        public ArrayList<LinkedList<Integer>> getParticles(){
+                
+                if(particles == null)
+                        return null;
+                
+                return new ArrayList<LinkedList<Integer>>(particles);
         }
 }
